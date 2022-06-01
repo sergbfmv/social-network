57bf99a7b36f0f1c0d0d3c3626bd93e8544afa41
Сергей <sergbfmv@mail.ru>
2022-06-01 14:56:58 +0300

    added Soree OOP and Observer
    refactoring of state on store OOP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state'
-import { renderEntireTree } from './render';
+import store from './redux/state'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-renderEntireTree(state)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function renderEntireTree() {
+  root.render(
+    <React.StrictMode>
+      <App
+        state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
+      />
+    </React.StrictMode>
+  );
+}
+renderEntireTree(store.getState())
+
+store.subscribe(renderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

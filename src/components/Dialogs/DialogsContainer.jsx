69d764778c3da683d03c56b4ef650031b8c6d07c
Сергей <sergbfmv@mail.ru>
2022-11-22 19:54:18 +0300

    marks

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import { onMessageChangeActionCreator, sendMessageActionCreator } from '../../redux/messagesReducer'
 import Dialogs from './Dialogs'
